b42215c9ff4bc5c34254e03d49583b4a97deff51
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-16 22:39:48 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 # Write your code here
-magic_count = 10
+magic_count = 11
 
 while magic_count > 0 do
-  puts magic_count
-  magic_count -= 1
+  puts magic_count - 1
+  #magic_count -= 1
 end
 puts "Happy New Year!"
